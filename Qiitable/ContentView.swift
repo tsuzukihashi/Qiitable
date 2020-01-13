08bff2f024c5d60cad945afb76c29f6f0c5effcd
tsuzukihashi <tsudukihashi0817@gmail.com>
2020-01-13 23:22:41 +0900

    Add Extensions

--- Qiitable/ContentView.swift
+++ Qiitable/ContentView.swift
@@ -2,7 +2,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Qiitable!")
+        ZStack {
+            Color.backgroundMain
+            Text("Hello, Qiitable!")
+        }
     }
 }
 
---
+++ Qiitable/Extensions/ColorExtensioin.swift
@@ -0,0 +1,5 @@
+import SwiftUI
+
+extension Color {
+    static let backgroundMain = Color("backgroundMain")
+}
