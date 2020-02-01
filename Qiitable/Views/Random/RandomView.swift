c1dbc029e819a6c2f59d575cea71d9c6dc064366
tsuzukihashi <tsudukihashi0817@gmail.com>
2020-02-01 16:39:02 +0900

    RandomItemRequestを追加

--- Qiitable/Views/Random/RandomView.swift
+++ Qiitable/Views/Random/RandomView.swift
@@ -3,6 +3,10 @@ import SwiftUI
 struct RandomView: View {
     var body: some View {
         Text("Random View")
+            .onAppear() {
+                let page = Int.random(in: 1...100)
+                let num = Int.random(in: 0...99)
+        }
     }
 }
 
