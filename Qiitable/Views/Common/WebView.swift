d34a84a38606e189993622c2677ba4a0431d3c85
tsuzukihashi <tsudukihashi0817@gmail.com>
2020-08-23 00:26:21 +0900

    WKNavigationDelegateの実装

--- Qiitable/Views/Common/WebView.swift
+++ Qiitable/Views/Common/WebView.swift
@@ -8,13 +8,32 @@ struct WebView: UIViewRepresentable {
         self.htmlBody = htmlBody
     }
 
-    func makeUIView(context: Context) -> WKWebView {
-        let view = WKWebView()
+    func makeUIView(context: UIViewRepresentableContext<WebView>) -> WKWebView {
+        let configure = WKWebViewConfiguration()
+        let preferences = WKPreferences()
+        configure.preferences = preferences
+        let view = WKWebView(frame: .zero, configuration: configure)
         view.allowsBackForwardNavigationGestures = true
+        view.navigationDelegate = context.coordinator
         return view
     }
 
     func updateUIView(_ uiView: WKWebView, context: Context) {
         uiView.loadHTMLString(htmlBody, baseURL: nil)
+    }
+
+    func makeCoordinator() -> WebView.Coordinator {
+        Coordinator()
+    }
+
+    class Coordinator: NSObject, WKNavigationDelegate {
+        // リクエスト前
+        func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+            decisionHandler(.allow)
+        }
+        // レスポンス取得後
+        func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+            decisionHandler(.allow)
+        }
     }
 }
