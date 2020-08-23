dc92775e67a3e67b45cec32c49120185afeef8ba
tsuzukihashi <tsudukihashi0817@gmail.com>
2020-08-23 17:56:05 +0900

    WebViewにCSSを適応

--- Qiitable/Views/Common/WebView.swift
+++ Qiitable/Views/Common/WebView.swift
@@ -35,5 +35,32 @@ struct WebView: UIViewRepresentable {
         func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
             decisionHandler(.allow)
         }
+
+        // 読み込み開始
+        func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        }
+
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            webView.evaluateJavaScript(getJS())
+        }
+
+        private func getJS() -> String {
+            return """
+            var newStyle = document.createElement('style');
+            newStyle.type = 'text/css';
+            document.getElementsByTagName('head').item(0).appendChild(newStyle);
+            css = document.styleSheets.item(0);
+            css.insertRule('body { background-color:#FFFAFA; }', 0);
+            css.insertRule('.code-frame { background-color:#000000; }', 0);
+            css.insertRule('.code-frame { color:#00FF00; }', 0);
+            css.insertRule('.code-frame { font-size: 40px; }', 0);
+
+            css.insertRule('p { font-size: 40px; }', 0);
+            css.insertRule('h1 { font-size: 52px; }', 0);
+            css.insertRule('h2 { font-size: 48px; }', 0);
+            css.insertRule('h3 { font-size: 44px; }', 0);
+            css.insertRule('ul { font-size: 40px; }', 0);
+            """
+        }
     }
 }
