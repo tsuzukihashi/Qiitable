3f9cd41cbc3993f0ab3bb99e600f48d7cb2ef7db
tsuzukihashi <tsudukihashi0817@gmail.com>
2020-08-28 23:05:48 +0900

    Makefileの追加

---
+++ prepare-for-develop.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+
+#brew
+if !(type "brew" > /dev/null 2>&1); then
+    echo "brew is not install. install homebrew. plz wait...."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+#mint
+if !(type "mint" > /dev/null 2>&1); then
+    echo "not install mint"
+    brew install mint
+fi
+
+exit 0
